57253a4cc317b534aae2b1f36101bab3d793b39e
ARAVIND-KUMAR-POTHULA <pothulaaravind241998@gmail.com>
2020-12-28 18:23:37 +0530

    ADD.added employee salary for 20 days file

--- empWageComputations.sh
+++ empWageComputations.sh
@@ -1,18 +1,27 @@
 #! /bin/bash -x
-isPartTime=1
-isFullTime=2
-isAbsent=0
-wagePerHour=20
+isFullTime=1
+isPartTime=2
 fullTimeHours=12
 partTimeHours=8
-employeeCheck=$(( RANDOM%3 ))
-	case $employeeCheck in 
-	$isFullTime)
-		dailyWage=$(( wagePerHour*fullTimeHours ))
-		;;
-	$isPartTime)
-                dailyWage=$(( wagePerHour*partTimeHours ))
-		;;
-		*)
-		dailyWage=0
-	esac
+absentHours=0
+empRatePerHr=20
+numWorkingDays=20
+days=1
+while [[ $days -le $numWorkingDays ]]
+do
+        empCheck=$((RANDOM%3))
+        case $empCheck in
+                $isFullTime)
+			totalSalary=$(( totalSalary+fullTimeHours*empRatePerHr))
+                        ;;
+                $isPartTime)
+			totalSalary=$(( totalSalary+partTimeHours*empRatePerHr))
+                        ;;
+                *)
+			totlSalary=$(( totalSalary+absentHours*empRatePerHr ))
+			;;
+        esac
+days=$(( days+1 ))
+done
+totalsalary=$(( empHrs*empRatePerHr))
+
